03ac909d97f2e470a501c49a702e38cd2f696f92
Luis Gonzalez <luisgzzrivas@gmail.com>
2018-03-03 11:00:07 -0600

    add lev and dev_lev distances

--- correction.py
+++ correction.py
@@ -1,6 +1,6 @@
-import pickle 
+import pickle
 
-with open('data/names.pickle', 'rb') as f: 
+with open('data/names.pickle', 'rb') as f:
 	NAMES = pickle.load(f)
 
 with open('data/lastnames.pickle', 'rb') as f:
@@ -14,27 +14,30 @@ def change_dict():
     if DICT == NAMES:
         DICT = LASTNAMES
         print('Dictionary changed to lastnames')
-    else: 
+    else:
         DICT = NAMES
         print('Dictionary changed to name')
 
 
-def P(name, N=sum(DICT.values())): 
+def P(name, N=sum(DICT.values())):
     "Probability of `name`."
     if name in DICT.keys():
         return DICT[name] / N
     else:
         return 0
 
-def correction(name): 
+
+def correction(name):
     "Most probable correction for name."
     return max(candidates(name.upper()), key=P)
 
-def candidates(name): 
+
+def candidates(name):
     "Generate possible spelling corrections for name."
     return (known([name]) or known(edits1(name)) or known(edits2(name)) or [name])
 
-def known(names): 
+
+def known(names):
     "The subset of `names` that appear in the dictionary of DICT."
     return set(w for w in names if w in DICT)
 
@@ -48,7 +51,6 @@ def edits1(name):
     inserts    = [L + c + R               for L, R in splits for c in letters]
     return set(deletes + transposes + replaces + inserts)
 
-def edits2(name): 
+def edits2(name):
     "All edits that are two edits away from `name`."
     return (e2 for e1 in edits1(name) for e2 in edits1(e1))
-
---
+++ funbetas.py
@@ -0,0 +1,91 @@
+import numpy as np
+
+letters = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def splits(string):
+    return [(string[:i], string[i:]) for i in range(len(string) + 1)]
+
+
+def transposes(string):
+    all_splits = splits(string)
+    return [L + R[1] + R[0] + R[2:] for L, R in all_splits if len(R) > 1]
+
+
+def deletes(string):
+    all_splits = splits(string)
+    return [L + R[1:] for L, R in all_splits if R]
+
+
+def replaces(string):
+    all_splits = splits(string)
+    return [L + c + R[1:] for L, R in all_splits if R for c in letters]
+
+
+def inserts(string):
+    all_splits = splits(string)
+    return [L + c + R for L, R in all_splits for c in letters]
+
+
+def lev(str1, str2, wheigts=(1, 1, 1)):
+    m = len(str1)
+    n = len(str2)
+    dist = np.zeros((m + 1, n + 1))
+
+    for i in range(m + 1):
+        dist[i][0] = i
+
+    for j in range(n + 1):
+        dist[0][j] = j
+
+    for j in range(n):
+        for i in range(m):
+            if str1[i] == str2[j]:
+                dist[i + 1][j + 1] = dist[i][j]
+            else:
+                dist[i + 1][j + 1] = min(dist[i][j + 1] + 1,
+                                         dist[i + 1][j] + 1,
+                                         dist[i][j] + 1)
+    return dist[m][n]
+
+
+def dam_lev(str1, str2, wheigts=(1, 1, 1)):
+    m = len(str1)
+    n = len(str2)
+    dist = np.zeros((m + 1, n + 1))
+
+    for i in range(m + 1):
+        dist[i][0] = i
+
+    for j in range(n + 1):
+        dist[0][j] = j
+
+    for j in range(n):
+        for i in range(m):
+            if str1[i] == str2[j]:
+                dist[i + 1][j + 1] = dist[i][j]
+            else:
+                dist[i + 1][j + 1] = min(dist[i][j + 1] + 1,
+                                         dist[i + 1][j] + 1,
+                                         dist[i][j] + 1)
+            if i > 0 and j > 0:
+                if str1[i] == str2[j - 1] and str1[i - 1] == str2[j]:
+                    dist[i + 1][j + 1] = min(dist[i + 1][j + 1],
+                                             dist[i - 1][j - 1] + 1)
+    return dist[m][n]
+
+
+dam_lev('AGUASCALIENTES', 'AGASCALIENTES')
+dam_lev('NUEVO LEON', 'AGASCALIENTES')
+dam_lev('COLIMA', 'AGASCALIENTES')
+dam_lev('ESTADO DE MEXICO', 'AGASCALIENTES')
+
+
+lev('LUIS', 'LIS')
+lev('LIZ', 'LIS')
+
+
+
+
+dam_lev('iL', 'Li')
+lev('Li', 'iL')
